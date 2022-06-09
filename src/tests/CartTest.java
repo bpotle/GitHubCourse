a63ee7804bc7a3ee62f14430c25ca2f1fa0605ef
Bharat Potle <bharatpotle11@gmail.com>
2022-06-09 20:13:48 +0530

    modified carttest file

--- src/tests/CartTest.java
+++ src/tests/CartTest.java
@@ -4,5 +4,6 @@ public class CartTest {
 
 	public void checkCart() {
 		System.out.println("Your Cart is tested Successfully....");
+		System.out.println("Your Carttest file is updated Successfully....");
 	}
 }
