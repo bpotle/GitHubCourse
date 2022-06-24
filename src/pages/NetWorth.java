a500be781e63366d5b9b864bc8e0b05027cafb57
bpotle <99797296+bpotle@users.noreply.github.com>
2022-06-24 16:02:05 +0530

    Update NetWorth.java
    added by Dev02

--- src/pages/NetWorth.java
+++ src/pages/NetWorth.java
@@ -3,6 +3,10 @@ package pages;
 public class NetWorth {
 	
 	public void test() {
+		System.out.println("This is NetWorth of Dev 02");
+		System.out.println("Networth validated by Dev 02");
+		System.out.println("Bye  Dev 02");
+		
 		
 		
 	}
