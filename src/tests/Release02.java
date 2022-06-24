9e243e026f187f168ec5ffd28a41a1065c257c61
bpotle <99797296+bpotle@users.noreply.github.com>
2022-06-24 13:39:11 +0530

    Update Release02.java

--- src/tests/Release02.java
+++ src/tests/Release02.java
@@ -1,5 +1,8 @@
 package tests;
 
 public class Release02 {
+  public void test(){
+    	System.out.println("This is done by Dev 02....");
+		System.out.println("This is validated by Dev 02....");
 
 }
