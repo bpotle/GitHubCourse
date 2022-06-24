73d50fc8f85eef172bb858a365c58d02e6158ee8
bpotle <99797296+bpotle@users.noreply.github.com>
2022-06-24 13:03:08 +0530

    Update Release01.java
    Code changes done by dev02

--- src/pages/Release01.java
+++ src/pages/Release01.java
@@ -2,7 +2,8 @@ package pages;
 
 public class Release01 {
 	public void test() {
-		
+		System.out.println("This is done by Dev02");
+		System.out.println("Release code validated by Dev 02...");
 	}
 
 }
