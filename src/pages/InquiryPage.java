40ee2a8ddfd0d7122ba176b1bf04a55be2aefe53
bharatpotle <bharatpotle1111@gmail.com>
2022-06-24 15:43:40 +0530

    added inquiry changes

---
+++ src/pages/InquiryPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class InquiryPage {
+	
+	public void test() {
+		System.out.println("This is inquiry page by Dev 1");
+		System.out.println("inquiry page validated by Dev 1");
+	}
+
+}
