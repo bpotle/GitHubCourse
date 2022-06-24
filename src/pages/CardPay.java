a02088800ca875f26cc31d0eb32f1c1dd402482e
bpotle <99797296+bpotle@users.noreply.github.com>
2022-06-24 12:12:59 +0530

    Update CardPay.java
    added changes to cardPay by developer 2

--- src/pages/CardPay.java
+++ src/pages/CardPay.java
@@ -1,5 +1,8 @@
 package pages;
 
 public class CardPay {
+  public void test(){
+    System.out.println("This is paid by card by dev-2");
+    System.out.println("Validation is successful by dev-2");
 
 }
