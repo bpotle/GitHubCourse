20bb2fcfc8a19a0c99432b4751b683d0152cce8f
bharatpotle <bharatpotle1111@gmail.com>
2022-06-25 22:33:15 +0530

    resolved conflict and kept dev01 changes

--- src/tests/ResponderPageTest.java
+++ src/tests/ResponderPageTest.java
@@ -5,6 +5,7 @@ public class ResponderPageTest {
 	public void test() {
 
 		System.out.println("this is dev01");
+<<<<<<< Updated upstream
 		System.out.println("this is updated by dev 02 welcome");
 
 		int a=3000;
@@ -13,6 +14,16 @@ public class ResponderPageTest {
 		System.out.println("total is: "+b);
 		System.out.println("this is updated by  dev 02 now...");
 		System.out.println("Welcome to world of constant change......");
+=======
+		System.out.println("this is updated by  dev 01 welcome");
+
+		int a=200;
+		int sal=121;
+		int b=a+sal;
+		System.out.println("total is: "+b);
+		System.out.println("this is updated by  dev 01 now...");
+		System.out.println("Welcome to world of constant change");
+>>>>>>> Stashed changes
 		
 
 		int l=2;
@@ -21,6 +32,7 @@ public class ResponderPageTest {
 		System.out.println("total is: "+m);
 		System.out.println("This changes are done by dev01");
 		System.out.println("Dev 02 changes removed and dev01 changes KEPT");
+		
 
 
 	}
