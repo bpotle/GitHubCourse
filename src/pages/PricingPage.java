c862c2ab0d2af3817d8cbbd9372b8a09be5b3324
bpotle <99797296+bpotle@users.noreply.github.com>
2022-06-25 20:07:27 +0530

    Update PricingPage.java

--- src/pages/PricingPage.java
+++ src/pages/PricingPage.java
@@ -9,5 +9,9 @@ public class PricingPage {
 		int b=11;
 		int c=a+b;
 		System.out.println("Total done by Devloper 1 is: "+c);
+		int x=222;
+		int y=4;
+		int z=x+y;
+		System.out.println("Total done by Devloper 1 is: "+z);
 	}
 }
