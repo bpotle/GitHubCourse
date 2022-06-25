e0574654ee9bd62cbd9a82e04e2a7f82dffba5d4
bharatpotle <bharatpotle1111@gmail.com>
2022-06-25 19:45:08 +0530

    added pricing code by dev01

---
+++ src/pages/PricingPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class PricingPage {
+
+	
+	public void test() {
+		
+		int a=10;
+		int b=11;
+		int c=a+b;
+		System.out.println("Total done by Devloper 1 is: "+c);
+	}
+}
