976e346ff0618ef88635ba4ddca3eb6accdc72f4
bharatpotle <bharatpotle1111@gmail.com>
2022-06-25 20:51:03 +0530

    updated respo by dev01

--- src/tests/ResponderPageTest.java
+++ src/tests/ResponderPageTest.java
@@ -6,10 +6,12 @@ public class ResponderPageTest {
 
 		System.out.println("this is dev01");
 		System.out.println("this is dev 01 welcome");
-		int a=101;
-		int sal=121;
 
-		
+		int a=100;
+		int sal=12;
+		int b=a+sal;
+		System.out.println("total is: "+b);
+
 	}
 	
 }
